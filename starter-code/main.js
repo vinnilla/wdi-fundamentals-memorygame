fa1f4653d1a43ec337512882c10500501d5acf5b
Vincent <vincentckhuang@gmail.com>
2016-06-15 15:20:46 -0700

    created alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,11 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if cardOne == cardTwo {
+	alert('You found a match!')
+}
+
+else {
+	alert('Sorry, try again.')
+}
